313dab2e03d38f618345104fae7518356c7cc8f5
Brittany Craig <brittanycraig@gmail.com>
2022-02-24 10:11:55 -0500

    fully working

--- word_frequency.py
+++ word_frequency.py
@@ -1,24 +1,13 @@
-from string import punctuation
-
-
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
-#don't change anything above this
 #import the python string library so we can get a list of punctuation 
+#from string import punctuation
 import string
 punctuation_list = string.punctuation
 #print(punctuation)
-#punctuation = punctuation_list.replace("", " ")
-#print("replace", punctuation)
-#punctuation = punctuation.split()
-#print("split", punctuation)
-# add the punctuation string items to the STOP_WORDS array
-#STOP_WORDS = STOP_WORDS + punctuation
-#print("This is STOP_WORDS:", STOP_WORDS)
-# our function
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
@@ -26,7 +15,6 @@ def print_word_freq(file):
     with open(file) as file:
         words = file.read()
         
-    words_dic = {}
     words = words.lower() #make lowercase
     words = words.rstrip()
     words = words.strip()
@@ -40,37 +28,35 @@ def print_word_freq(file):
     #print(words)
 
     words = words.split()
-    #words = [x.strip() for x in words.split()] Nope didn't need this, too complex! above does same
-    #print("after split:", words) 
-    #print(type(words))
-    
+
     #remove stop_words
-    #print(words_copy)
     words_2 = []
-    #print(len(words))
-    #print(len(words_2))
-    # this is only removing the first incidence of the element :(
-    #while len(words_2) <= len(words):
     for i in words:
         if i not in STOP_WORDS:
             #words_2 += i #nope counting letters or something!
             words_2.append(i) 
         #print(words_2)  
-    words = words_2         
-    #print(len(words_2))      
-    #print(words_2)        
-    #words = words_2
-    #print("stopwords removed:", words)
-   
-    #This counts the frequency of the words and adds the key/value pairs to our dictionary
+    words = words_2   #putting it back into words
+
+    #This counts the frequency of the words and adds the key/value pairs to the dictionary
     #List comprehension?? - https://www.educative.io/edpresso/how-to-count-the-number-of-occurrences-of-a-list-item-in-python
-    words_dic = (dict((x,words.count(x)) for x in set(words)))
-    #print("Here's the dictionary:", words_dic)
-    #print(words_dic.keys())
-    
-    #print(sorted(words_dic.items(), key=lambda seq: seq[1]))
+    words_dic = {}
+    words_dic = (dict((i,words.count(i)) for i in set(words)))
+    print(type(words_dic)) #confirmed- it's a dictionary!
+    #print(words_dic.keys()) 
+    #sorting
     for key, value in sorted(words_dic.items(), key=lambda seq: seq[1], reverse=True):
-        print(f"{key} | {value}")
+        #thank you Emily for this line of code!
+        print(f"{key:>20} | {(value * '*'):<20}")
+    
+    #print(len(words))
+    #print(len(words_2))
+    # this is only removing the first incidence of the element :(
+    #while len(words_2) <= len(words
+    #words = [x.strip() for x in words.split()] Nope didn't need this, too complex! above does same
+    #print("after split:", words) 
+    #print(type(words))    
+        
     #print(sorted(words_dic, reverse=True))
     #removes duplicate words, but how to make count? 
     #for i in words:
@@ -86,7 +72,14 @@ def print_word_freq(file):
 #words = file() #can we change this to words?
 #def count_words(string):    
         
-
+#punctuation = punctuation_list.replace("", " ")
+#print("replace", punctuation)
+#punctuation = punctuation.split()
+#print("split", punctuation)
+# add the punctuation string items to the STOP_WORDS array
+#STOP_WORDS = STOP_WORDS + punctuation
+#print("This is STOP_WORDS:", STOP_WORDS)
+# our function
 # don't change anything below this
 if __name__ == "__main__":
     import argparse
